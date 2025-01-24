0f20b31d21c6c8599d9e3ad393d93c7b9a264e38
Mode Zitraks <artembbarr@gmail.com>
2025-01-24 17:01:21 +0300

    выполнена 2 задача 2 урока

---
+++ Task_02_02/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_02_02
+{
+    using System;
+    class Program
+    {
+        static void Main()
+        {
+            // Заданные значения
+            double a = 8;
+            double b = 14;
+            double c = Math.PI / 4; // c = π/4
+
+            // Вычисление выражения
+            double numerator = Math.Sqrt(b + 3 * (a - 1));
+            double denominator = Math.Abs(a - b * (Math.Sin(c) * Math.Sin(c) + Math.Tan(c)));
+
+            double result = numerator / denominator;
+
+            // Вывод результата
+            Console.WriteLine("Результат: " + result);
+        }
+    }
+
+}
