f0e6215469c2d1cf3b425cbad108fc90630ca738
Mode Zitraks <artembbarr@gmail.com>
2025-03-04 16:59:21 +0300

    выполнено 1 задание 11 пары

---
+++ Task_11_01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class PassByValueExample
+{
+    public static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 20;
+
+        Console.WriteLine($"Значения до вызова метода: a = {a}, b = {b}");
+
+        Swap(a, b);
+
+        Console.WriteLine($"Значения после вызова метода: a = {a}, b = {b}");
+    }
+
+    // метод для обмена значениями двух целых чисел
+    public static void Swap(int x, int y)
+    {
+        Console.WriteLine($"  Значения внутри метода до обмена: x = {x}, y = {y}");
+
+        int temp = x;
+        x = y;
+        y = temp;
+
+        Console.WriteLine($"  Значения внутри метода после обмена: x = {x}, y = {y}");
+    }
+}
