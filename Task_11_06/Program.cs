9af19ba4cafb99559fd3dc964972ff6c01f3cfe8
Mode Zitraks <artembbarr@gmail.com>
2025-03-04 17:33:15 +0300

    сделано 6 задание 11 урока

---
+++ Task_11_06/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class ArrayManipulation
+{
+    public static void Main(string[] args)
+    {
+        int[] originalArray = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Исходный массив:");
+        PrintArray(originalArray);
+
+        IncreaseArrayElements(originalArray);
+
+        Console.WriteLine("\nМассив после вызова метода:");
+        PrintArray(originalArray);
+    }
+
+    // Метод для увеличения каждого элемента массива на 1
+    public static void IncreaseArrayElements(int[] array)
+    {
+        Console.WriteLine("  Внутри метода:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i]++; // Увеличение элемента массива
+            Console.Write($" {array[i]}");
+        }
+        Console.WriteLine();
+    }
+
+    // вывод массива на консоль.
+    public static void PrintArray(int[] array)
+    {
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+    }
+}
