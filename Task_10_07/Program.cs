a2dc103291066301d75fde398508c934ba0d38c1
Mode Zitraks <artembbarr@gmail.com>
2025-03-04 16:50:44 +0300

    выполнена 7 задача 10 урока

---
+++ Task_10_07/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class CharArrayGenerator
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество строк:");
+        int rows = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите количество столбцов:");
+        int cols = int.Parse(Console.ReadLine());
+
+        char[,] array = GenerateRussianCharArray(rows, cols);
+        PrintCharArray(array);
+    }
+
+    // метод для генерации двумерного символьного массива
+    public static char[,] GenerateRussianCharArray(int rows, int cols)
+    {
+        char[,] array = new char[rows, cols];
+        Random random = new Random();
+
+        // Диапазон символов русского алфавита (от 'а' до 'я')
+        int start = (int)'а';
+        int end = (int)'я';
+        int range = end - start + 1;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                // генерируем случайный индекс для буквы в русском алфавите
+                int randomIndex = random.Next(range);
+                array[i, j] = (char)(start + randomIndex); // преобразуем индекс обратно в символ
+            }
+        }
+
+        return array;
+    }
+
+    // метод для вывода двумерного символьного массива на консоль.
+    public static void PrintCharArray(char[,] array)
+    {
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+
+        Console.WriteLine("Сгенерированный символьный массив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
