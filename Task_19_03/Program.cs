34e6bfc075a2051d8359dda1ec90e99721171d38
Mode Zitraks <artembbarr@gmail.com>
2025-04-07 15:32:46 +0300

    сделана 3 задача 19 лекции

---
+++ Task_19_03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StringCombiner
+{
+    public static void Main(string[] args)
+    {
+        List<string> inputLines = new List<string>();
+        string line;
+        int lineNumber = 1;
+
+        Console.WriteLine("Введите строки (для завершения введите пустую строку):");
+
+        do
+        {
+            Console.Write($"Введите строку {lineNumber}: ");
+            line = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(line))
+            {
+                inputLines.Add(line);
+                lineNumber++;
+            }
+
+        } while (!string.IsNullOrEmpty(line));
+
+        string resultString = string.Join("-", inputLines);
+
+        Console.WriteLine($"Результат: {resultString}");
+    }
+}
+
