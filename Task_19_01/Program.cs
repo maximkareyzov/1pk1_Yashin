6d5f8492fcd7ecd58703565c3fa7f3d57accc8de
Mode Zitraks <artembbarr@gmail.com>
2025-04-07 15:24:58 +0300

    сделана 1 задача 19 лекции

---
+++ Task_19_01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class StringReplacer
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку:");
+        string inputString = Console.ReadLine();
+
+        Console.WriteLine("Введите подстроку для поиска:");
+        string searchString = Console.ReadLine();
+
+        Console.WriteLine("Введите подстроку для замены:");
+        string replaceString = Console.ReadLine();
+
+        string resultString = inputString.Replace(searchString, replaceString);
+
+        Console.WriteLine("Результат: " + resultString);
+    }
+}
